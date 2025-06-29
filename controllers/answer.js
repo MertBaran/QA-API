f74239d982a5de4056db9baf9061cc72df1972f7
Mert Baran <32373915+MertBaran@users.noreply.github.com>
2025-06-29 07:01:39 +0300

    config revision

--- controllers/answer.js
+++ controllers/answer.js
@@ -3,122 +3,125 @@ const Answer = require("../models/Answer");
 const CustomError = require("../helpers/error/CustomError.js");
 const asyncErrorWrapper = require("express-async-handler");
 
-const addNewAnswerToQuestion = asyncErrorWrapper(async (req,res,next) => {
-    const {question_id} = req.params;
-    const user_id = req.user.id;
-    const information = req.body;
+const addNewAnswerToQuestion = asyncErrorWrapper(async (req, res, next) => {
+  const { question_id } = req.params;
+  const user_id = req.user.id;
+  const information = req.body;
 
-    const answer = await Answer.create({
-        ...information,
-        question : question_id,
-        user : user_id
-    });
+  const answer = await Answer.create({
+    ...information,
+    question: question_id,
+    user: user_id,
+  });
 
-    return res.status(200).json({
-        success : true,
-        data : answer
-    });
+  return res.status(200).json({
+    success: true,
+    data: answer,
+  });
 });
 
-const getAllAnswersByQuestion = asyncErrorWrapper(async (req,res,next) => {
-    const {question_id} = req.params;
-    const answers = await Answer.find({question : question_id});
-    return res.status(200).json({
-        success : true,
-        data : answers
-    });
+const getAllAnswersByQuestion = asyncErrorWrapper(async (req, res, next) => {
+  const { question_id } = req.params;
+  const answers = await Answer.find({ question: question_id });
+  return res.status(200).json({
+    success: true,
+    data: answers,
+  });
 });
 
-const getSingleAnswer = asyncErrorWrapper(async (req,res,next) => {
-    const {answer_id} = req.params;
-    const answer = await Answer.findById(answer_id)
+const getSingleAnswer = asyncErrorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
+  const answer = await Answer.findById(answer_id)
     .populate({
-        path : "user",
-        select : "name profile_image"
+      path: "user",
+      select: "name profile_image",
     })
     .populate({
-        path : "question",
-        select : "title"
+      path: "question",
+      select: "title",
     });
-    return res.status(200).json({
-        success : true,
-        data : answer
-    });
+  return res.status(200).json({
+    success: true,
+    data: answer,
+  });
 });
 
-const editAnswer = asyncErrorWrapper(async (req,res,next) => {
-    const {answer_id} = req.params;
-    const {content} = req.body;
+const editAnswer = asyncErrorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
+  const { content } = req.body;
 
-    let answer = await Answer.findById(answer_id);
-    let old_content = answer.content; //ben yaptım denemek için
-    answer.content = content;
+  let answer = await Answer.findById(answer_id);
+  let old_content = answer.content; //ben yaptım denemek için
+  answer.content = content;
 
-    await answer.save();
+  await answer.save();
 
-    return res.status(200).json({
-        success : true,
-        data : answer,
-        old_content : old_content
-    });
+  return res.status(200).json({
+    success: true,
+    data: answer,
+    old_content: old_content,
+  });
 });
 
-const deleteAnswer = asyncErrorWrapper(async (req,res,next) => {
-    const {answer_id} = req.params;
-    const {question_id} = req.params;
-    
-    await Answer.findByIdAndDelete(answer_id);
+const deleteAnswer = asyncErrorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
+  const { question_id } = req.params;
 
-    const question = await Question.findById(question_id);
-    question.answers.splice(question.answers.indexOf(answer_id), 1);
-    await question.save();
+  await Answer.findByIdAndDelete(answer_id);
 
-    return res.status(200).json({
-        success : true,
-        message : "Answer deleted successfully"
-    });
+  const question = await Question.findById(question_id);
+  question.answers.splice(question.answers.indexOf(answer_id), 1);
+  await question.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Answer deleted successfully",
+  });
 });
 
+const likeAnswer = asyncErrorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
 
-const likeAnswer = asyncErrorWrapper(async (req, res,next) => {
-    const {id} = req.params;
+  const answer = await Answer.findById(answer_id);
+  // Like etmişse
+  if (answer.likes.includes(req.user.id)) {
+    return next(new CustomError("You already like this answer", 400));
+  }
+  answer.likes.push(req.user.id);
+  await answer.save();
 
-    const question = await Question.findById(id);
-    // Like etmişse
-    if(question.likes.includes(req.user.id)){
-        return next(new CustomError("You already like this question",400));
-    }
-    question.likes.push(req.user.id);
-    await question.save();
-
-    return res.status(200).json({
-        success: true,
-        data: question
-    });
+  return res.status(200).json({
+    success: true,
+    data: answer,
+  });
 });
 
-const undoLikeQuestion = asyncErrorWrapper(async (req, res,next) => {
-    const {id} = req.params;
+const undoLikeAnswer = asyncErrorWrapper(async (req, res, next) => {
+  const { answer_id } = req.params;
 
-    const question = await Question.findById(id);
-    // Like etmemişse
-    if(!question.likes.includes(req.user.id)){
-        return next(new CustomError("You can not undo like operation for this question",400));
-    }
-    const index = question.likes.indexOf(req.user.id);
-    question.likes.splice(index,1);
-    await question.save();
-    
-    return res.status(200).json({
-        success: true,
-        data: question
-    });
+  const answer = await Answer.findById(answer_id);
+  // Like etmemişse
+  if (!answer.likes.includes(req.user.id)) {
+    return next(
+      new CustomError("You can not undo like operation for this answer", 400)
+    );
+  }
+  const index = answer.likes.indexOf(req.user.id);
+  answer.likes.splice(index, 1);
+  await answer.save();
+
+  return res.status(200).json({
+    success: true,
+    data: answer,
+  });
 });
 
 module.exports = {
-    addNewAnswerToQuestion,
-    getAllAnswersByQuestion,
-    getSingleAnswer,
-    editAnswer,
-    deleteAnswer
-}
+  addNewAnswerToQuestion,
+  getAllAnswersByQuestion,
+  getSingleAnswer,
+  editAnswer,
+  deleteAnswer,
+  likeAnswer,
+  undoLikeAnswer,
+};
--- server.js
+++ server.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const routers = require('./routers');
-const connectDatabase = require('./helpers/database/connectDatabase');
-const customErrorHandler = require('./middlewares/errors/customErrorHandler');
-const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const routers = require("./routers");
+const connectDatabase = require("./helpers/database/connectDatabase");
+const customErrorHandler = require("./middlewares/errors/customErrorHandler");
+const path = require("path");
 
 //Environment Variables
 dotenv.config({
-    path: './config/env/config.env'
+  path: "./config/env/config.env",
 });
 //Mongo Database Connection
 connectDatabase();
 const app = express();
 
+//CORS Middleware
+app.use(
+  cors({
+    origin: true, // Allow all origins for development
+    credentials: true,
+  })
+);
+
 //Body Middleware
 app.use(express.json());
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 //Routers Middleware
-app.use('/api', routers);
+app.use("/api", routers);
 
 //Error Handler
 app.use(customErrorHandler);
 
 //Static Files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} : ${process.env.NODE_ENV}`);
+  console.log(`Server is running on port ${PORT} : ${process.env.NODE_ENV}`);
 });
-
