83b13b716fbd7ae0605382a3523bfab8a088e047
Mert Baran <32373915+MertBaran@users.noreply.github.com>
2025-10-28 15:40:40 +0300

    Move input helpers to infrastructure/validation

---
+++ infrastructure/validation/inputHelpers.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+
+const validateUserInput = (email: string, password: string): boolean => {
+  return email && password ? true : false;
+};
+
+const comparePassword = (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export { validateUserInput, comparePassword };
